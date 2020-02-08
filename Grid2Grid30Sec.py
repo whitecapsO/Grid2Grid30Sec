308d2665f623264588ba181844eb5647e338a797
whitecapsO <36182802+whitecapsO@users.noreply.github.com>
2020-02-09 11:49:35 +1300

    Created

---
+++ Grid2Grid30Sec.py
@@ -0,0 +1,162 @@
+from farmware_tools import app
+from farmware_tools import device
+from farmware_tools import env
+from farmware_tools import get_config_value
+
+# Rewrite of Grid2Grid to run in 30 seconds due to limitations put on Farmware 
+# i.e. Farmware can only run for 30 seconds and there is a 2 second delay between device calls
+# the only way to loop is to use sequence recursion
+# There will be three calls to the device to stay within the 30 second window
+# 1 - Get the current coordinates
+# 2 - Once the next co-ordinates are identified based on the current co-ordinates move there
+# 3 - Log the move
+
+# Decide if you pass a start flag for the first grid move on subsequent calls set this to false
+# May need to pass the current x, y, z co-ordinates rather than make the first call for the current co-ordinates
+
+# Remember if using alternate inbetween last row is missed so:
+# Normal grid: 3 rows x 2 columns = 6 cells
+# Alternate in between grid: 2 rows x 4 columns = 6 cells as last rows 2 of alternate inbetween columns missed
+# Not tested turning alternate inbetween on both grids at the same time
+# A better way would be to initialise 2 arrays with x,y coordinates and loop through them but this algo works
+
+# Values for testing
+# *** First grid ***
+# rowsGrid1 = 4
+# colsGrid1 = 7 
+# spaceBetweenRowsGrid1 = 47
+# spaceBetweenColsGrid1 = 45 
+# startXGrid1 = 310.2
+# startYGrid1 = 563.8
+# startZGrid1 = 210.96
+# alternateInBetweenGrid1 = false
+# 
+# rowsGrid2 = 4
+# colsGrid2 = 7
+# spaceBetweenRowsGrid2 = 140
+# spaceBetweenColsGrid2 = 200 
+# startXGrid2 = 
+# startYGrid2 = 
+# startZGrid2 = 
+# alternateInBetweenGrid2 = false
+
+rowsGrid1 = get_config_value(farmware_name='Grid2Grid30Sec', config_name='rowsGrid1', value_type=int)
+colsGrid1 = get_config_value(farmware_name='Grid2Grid30Sec', config_name='colsGrid1', value_type=int)
+spaceBetweenRowsGrid1 = get_config_value(farmware_name='Grid2Grid30Sec', config_name='spaceBetweenRowsGrid1', value_type=float)
+spaceBetweenColsGrid1 = get_config_value(farmware_name='Grid2Grid30Sec', config_name='spaceBetweenColsGrid1', value_type=float)
+startXGrid1 = get_config_value(farmware_name='Grid2Grid30Sec', config_name='startXGrid1', value_type=float)
+startYGrid1 = get_config_value(farmware_name='Grid2Grid30Sec', config_name='startYGrid1', value_type=float)
+startZGrid1 = get_config_value(farmware_name='Grid2Grid30Sec', config_name='startZGrid1', value_type=float)
+alternateInBetweenGrid1 = get_config_value(farmware_name='Grid2Grid30Sec', config_name='alternateInBetweenGrid1', value_type=int)
+startLastRowOfGrid1 = get_config_value(farmware_name='Grid2Grid30Sec', config_name='startLastRowOfGrid1', value_type=int)
+
+rowsGrid2 = get_config_value(farmware_name='Grid2Grid30Sec', config_name='rowsGrid2', value_type=int)
+colsGrid2 = get_config_value(farmware_name='Grid2Grid30Sec', config_name='colsGrid2', value_type=int)
+spaceBetweenRowsGrid2 = get_config_value(farmware_name='Grid2Grid30Sec', config_name='spaceBetweenRowsGrid2', value_type=float)
+spaceBetweenColsGrid2 = get_config_value(farmware_name='Grid2Grid30Sec', config_name='spaceBetweenColsGrid2', value_type=float)
+startXGrid2 = get_config_value(farmware_name='Grid2Grid30Sec', config_name='startXGrid2', value_type=float)
+startYGrid2 = get_config_value(farmware_name='Grid2Grid30Sec', config_name='startYGrid2', value_type=float)
+startZGrid2 = get_config_value(farmware_name='Grid2Grid30Sec', config_name='startZGrid2', value_type=float)
+alternateInBetweenGrid2 = get_config_value(farmware_name='Grid2Grid30Sec', config_name='alternateInBetweenGrid2', value_type=int)
+
+# Initialise row (X) and column (Y) indexes for all grids
+rowGrid1Index = 0
+colGrid1Index = 0
+rowGrid2Index = 0
+colGrid2Index = 0
+
+# Initialise the current Position Found flags to not found
+currentPositionGrid1Found = False
+currentPositionGrid2Found = False
+
+# Set constant Z positions
+zPosGrid1 = startZGrid1
+zPosGrid2 = startZGrid2
+
+# Get the current position
+currentPosition = device.POSITION
+
+# Start the first grid movement
+for rowGrid1Index in range(rowsGrid1):
+    # Set first grids y position back to the first column
+    yPosGrid1 = startYGrid1
+
+    for colGrid1Index in range(colsGrid1):
+        # Set the x and y positions on the first grid if alternateInBetween assume the first 
+        # column is not an alternateInBetween then odd numbered colums are
+        # if startLastRowOfGrid1 then the x position starts on the last row and moves backwards
+        if alternateInBetweenGrid1 == 1 :
+            if colGrid1Index > 0 and (colGrid1Index % 2) > 0 :
+                device.log(message='Grid 1 alternateInBetween', message_type='success')
+                if startLastRowOfGrid1 == 1 :
+                    xPosGrid1 = startXGrid1 - (spaceBetweenRowsGrid1 * 0.5) - (spaceBetweenRowsGrid1 * rowGrid1Index)
+                else :
+                    xPosGrid1 = startXGrid1 + (spaceBetweenRowsGrid1 * 0.5) + (spaceBetweenRowsGrid1 * rowGrid1Index)
+            else :
+                if startLastRowOfGrid1 == 1 :
+                    xPosGrid1 = startXGrid1 - (spaceBetweenRowsGrid1 * rowGrid1Index)
+                else :
+                    xPosGrid1 = startXGrid1 + (spaceBetweenRowsGrid1 * rowGrid1Index)
+        else :
+            if startLastRowOfGrid1 == 1 :
+                xPosGrid1 = startXGrid1 - (spaceBetweenRowsGrid1 * rowGrid1Index)                    
+            else :
+                xPosGrid1 = startXGrid1 + (spaceBetweenRowsGrid1 * rowGrid1Index)
+
+        # 1st grid move set the first grid row index back to zero if alternate inbetween column on last row let the loop handle the rest
+        if ((alternateInBetweenGrid1 == 1)                  # Is alternateInBetween
+        and (colGrid1Index > 0 and (colGrid1Index % 2) > 0) # is on an alternateInBetween odd numbered (offset) column  
+        and (rowGrid1Index >= rowsGrid1 - 1)) :             # is on the second to last row index as an alternateInBetween has 1 less row
+            # Increment y column position for grid 1
+            yPosGrid1 = yPosGrid1 + spaceBetweenColsGrid1
+            #device.log(message='Grid 1 alternateInBetween column last row so miss a row', message_type='success')
+        else :
+            # If the second grid was found then move otherwise check if we've reached the current position
+            if currentPositionGrid2Found :
+                currentPositionGrid2Found = false
+                device.move_absolute(
+                device.assemble_coordinate(xPosGrid1, yPosGrid1, zPosGrid1),
+                100,
+                device.assemble_coordinate(0, 0, 0))
+                device.log('Grid 1 moving to ' + str(xPosGrid1) + ', ' + str(yPosGrid1) + ', ' + str(zPosGrid1), 'success', ['toast'])
+            elif ((xPosGrid1 - 5)  <= currentPosition['x'] <= (xPosGrid1 + 5)) and ((yPosGrid1 - 5)  <= currentPosition['y'] <= (yPosGrid1 + 5)) :
+                currentPositionGrid1Found
+
+            # Set the x and y positions on the second grid if alternateInBetween assume the first 
+            # column is not an alternateInBetween then odd numbered colums are
+            if alternateInBetweenGrid2 == 1 :
+                if colGrid2Index > 0 and (colGrid2Index % 2) > 0 :
+                    device.log(message='Grid 2 alternateInBetween column', message_type='success')
+                    xPosGrid2 = startXGrid2 + (spaceBetweenRowsGrid2 * 0.5) + (spaceBetweenRowsGrid2 * rowGrid2Index)
+                else :
+                    xPosGrid2 = startXGrid2 + (spaceBetweenRowsGrid2 * rowGrid2Index)
+            else :
+                xPosGrid2 = startXGrid2 + (spaceBetweenRowsGrid2 * rowGrid2Index)
+            
+            yPosGrid2 = startYGrid2 + (spaceBetweenColsGrid2 * colGrid2Index)
+
+            # If the first grid was found then move otherwise check if we've reached the current position
+            if currentPositionGrid1Found :
+                currentPositionGrid1Found = false
+                device.move_absolute(
+                device.assemble_coordinate(xPosGrid2, yPosGrid2, zPosGrid2),
+                100,
+                device.assemble_coordinate(0, 0, 0))
+                device.log('Grid 2 moving to ' + str(xPosGrid2) + ', ' + str(yPosGrid2) + ', ' + str(zPosGrid2), 'success', ['toast'])
+            elif ((xPosGrid2 - 5)  <= currentPosition['x'] <= (xPosGrid2 + 5)) and ((yPosGrid2 - 5)  <= currentPosition['y'] <= (yPosGrid2 + 5)) :
+                currentPositionGrid2Found
+
+            # Increment y column position for grid 1
+            yPosGrid1 = yPosGrid1 + spaceBetweenColsGrid1
+
+            # Set the second grid row and column indexes
+            if ((alternateInBetweenGrid2 == 1)                  # Is alternateInBetween
+            and (colGrid2Index > 0 and (colGrid2Index % 2) > 0) # is on an alternateInBetween odd numbered (offset) column  
+            and (rowGrid2Index >= rowsGrid2 - 2)) :              # is on the second to last row index as an alternateInBetween has 1 less row
+                rowGrid2Index = 0                                   # Reset row index
+                colGrid2Index += 1                                  # Increment column index to move to the next column
+            elif rowGrid2Index >= (rowsGrid2 - 1) :             # else if on the last row
+                rowGrid2Index = 0                                   # Reset row index
+                colGrid2Index += 1                                  # Increment column index to move to the next column
+            else :                                              # else
+                rowGrid2Index += 1                                  # Increment row index to move to the next row
