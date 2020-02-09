26b925473dfaa075c3b5def2101ea634b92d82a1
whitecapsO <36182802+whitecapsO@users.noreply.github.com>
2020-02-09 14:40:53 +1300

    Debug

--- Grid2Grid30Sec.py
+++ Grid2Grid30Sec.py
@@ -55,7 +55,7 @@ zPosGrid2 = startZGrid2
 
 # Get the current position
 currentPosition = device.get_current_position()
-device.log(message='Get the ints', message_type='success')
+device.log(message='Get the ints ' + type(currentPosition['x']), message_type='success')
 currentPositionX = int(currentPosition['x'])
 currentPositionY = int(currentPosition['y'])
 currentPositionZ = int(currentPosition['z'])
